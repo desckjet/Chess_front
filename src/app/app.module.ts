c48e78ffdd961b72eb907cc5d5a15dad8feabe0b
David Acevedo <desckjetd@gmail.com>
2019-05-06 20:13:19 -0500

    se añaden los turnos y la comunicacion back - front
    se obtiene el estado del tablero, se añaden los turnos de los peones y se realiza la coneccion back - front

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
 import { TableroComponent } from './tablero/tablero.component';
+import { OperacionesComponent } from './operaciones/operaciones.component';
+
 import { PeonService } from './services/peon.service';
 import { TorreService } from './services/torre.service';
 import { CaballoService } from './services/caballo.service';
 import { AlfilService } from './services/alfil.service';
 import { DamaService } from './services/dama.service';
 import { ReyService } from './services/rey.service';
-import { OperacionesComponent } from './operaciones/operaciones.component';
+import { DataService } from './services/data.service';
 
 @NgModule({
   declarations: [
@@ -20,9 +24,10 @@ import { OperacionesComponent } from './operaciones/operaciones.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
   ],
-  providers: [PeonService, TorreService, CaballoService, AlfilService, DamaService, ReyService],
+  providers: [PeonService, TorreService, CaballoService, AlfilService, DamaService, ReyService, DataService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
